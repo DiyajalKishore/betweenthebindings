9601c24cb46be0209082316c412d3e9c9fae3ac4
Diyajal Kishore <kishore.diyajal@gmail.com>
2026-09-17 00:00:46 +0200

    Fix Vite multi-page entry config

--- vite.config.js
+++ vite.config.js
@@ -8,13 +8,12 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
         about: resolve(__dirname, 'about.html'),
         art: resolve(__dirname, 'art.html'),
-        buriedbetweenbindings: resolve(__dirname, 'buriedbetweenbindings.html'),
+        buriedbetweenthebindings: resolve(__dirname, 'buriedbetweenbindings.html'),
         creatingcommunity: resolve(__dirname, 'creatingcommunity.html'),
         griefgathersandgrows: resolve(__dirname, 'griefgathersandgrows.html'),
         melvilleartmile: resolve(__dirname, 'melvilleartmile.html'),
         thunee: resolve(__dirname, 'thunee.html'),
         writing: resolve(__dirname, 'writing.html'),
-        story: resolve(__dirname, 'story.html')
       }
     }
   }
